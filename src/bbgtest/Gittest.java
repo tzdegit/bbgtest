bb1e6b5cad3d31105ddfe7a9c6245fa920007783
tzdegit <eeds_taoze@sina.com>
2019-05-19 13:02:14 +0800

    gx

---
+++ src/bbgtest/Gittest.java
@@ -0,0 +1,10 @@
+package bbgtest;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		System.out.println("lalalla");
+		System.out.println("sssssss");
+		System.out.println("sssssss");
+	}
+}
